1a989b919d66f9caf1bb32698cb6f6a15c026799
THIAGO GOMES DE BARROS <thiago.barros@terceirizado.credisis.com.br>
2024-12-11 14:23:55 -0300

    Ajustes

--- analisedeclaracaotjba.client/src/app/services/documentos.service.ts
+++ analisedeclaracaotjba.client/src/app/services/documentos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { catchError, map, Observable, of, throwError } from 'rxjs';
 import { Documento } from '../models/documento.model';
+
 
 @Injectable({
   providedIn: 'root',
